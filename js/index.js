d2029b0d555a984c6b6c89a00b969b228a47b5db
Siarhei Drozd <s.drozd@cheil.com>
2020-07-16 17:24:46 +0200

    added: scroll-top elelment

--- js/index.js
+++ js/index.js
@@ -7,3 +7,41 @@ navItems.forEach((navItem) => {
 		selectInput.checked = false;
 	});
 });
+
+const offset = 100;
+const scrollUpEl = document.querySelector(".scroll-up");
+const scrollUpSvgPath = document.querySelector(".scroll-up__svg-path");
+const pathLength = scrollUpSvgPath.getTotalLength();
+
+scrollUpSvgPath.style.strokeDasharray = `${pathLength} ${pathLength}`;
+scrollUpSvgPath.style.transition = "stoke-dashoffset 20ms";
+
+// update dashhoffset
+const updateDashoffset = () => {
+  const height = document.documentElement.scrollHeight - window.innerHeight
+  const dashoffset = pathLength - (getTop() * pathLength / height)
+
+  scrollUpSvgPath.style.strokeDashoffset = dashoffset;
+};
+
+const getTop = () => window.pageYOffset || document.documentElement.scrollTop;
+
+//onScroll
+window.addEventListener("scroll", () => {
+  updateDashoffset();
+  
+	if (getTop() > offset) {
+		scrollUpEl.classList.add("scroll-up--visible");
+	} else {
+		scrollUpEl.classList.remove("scroll-up--visible");
+	}
+});
+
+//onClick
+
+scrollUpEl.addEventListener("click", () => {
+	window.scrollTo({
+		top: 0,
+		behavior: "smooth",
+	});
+});
