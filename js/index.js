2b60dfb6ba1a31323afa8a4ce73ca58917dba71c
Siarhei Drozd <s.drozd@cheil.com>
2020-07-16 15:07:42 +0200

    added: animation on scroll

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
-const navItems = document.querySelectorAll('.hamburger__navigation-element');
-const selectInput = document.getElementById('hamburger');
+AOS.init();
+const navItems = document.querySelectorAll(".hamburger__navigation-element");
+const selectInput = document.getElementById("hamburger");
 
-navItems.forEach(navItem => {
-  navItem.addEventListener('click', () => {
-    selectInput.checked = false;
-  })
-})
+navItems.forEach((navItem) => {
+	navItem.addEventListener("click", () => {
+		selectInput.checked = false;
+	});
+});
